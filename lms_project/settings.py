ec0763e8b2d77a7a6926fe09ad0ed1b936b3c90e
Salih Hayat <106287850+SalihHayat74@users.noreply.github.com>
2026-05-06 22:53:05 +0300

    removed sqlite db

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'lms_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#         conn_max_age=600
+#     )
+# }
 
 
 # Password validation
